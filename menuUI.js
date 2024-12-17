8fb857470a998f9caf2a92c5069c820a7de51002
taramcan <tara@thenextprime.com>
2024-12-17 14:21:55 -0600

    removed draft files

--- menuUI.js
+++
@@ -1,8 +0,0 @@
-function onOpen() {
-  const ui = SpreadsheetApp.getUi();
-  const menu = ui.createMenu('SATeam Workflow');
-  menu.addItem('Compile data fror 300R', 'populate300R')
-  menu.addItem('Populate 300R', 'populate300RDocument')
-  menu.addItem('Send Request for FollowUp Data', 'followUpEmailInputRequest')
-  menu.addToUi();
-}
